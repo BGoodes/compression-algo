1c11a5c09cbe1baa5fab2fde25f56d3a753a4984
Julien Moullec <contact@julien-moullec.fr>
2025-03-23 13:22:34 +0100

    Implement dct functions

---
+++ decompress/apply_idct.m
@@ -0,0 +1,16 @@
+% function [compY, compU, compV]=apply_idct(dctY, dctU, dctV)
+%
+% Computes the inverse DCT of the YUV components
+% Input:
+%    dctY, dctU et dctV : DCT of the YUV components
+%
+% Outputs:
+%    compY, compU et compV : YUV components of the image
+
+function [compY, compU, compV] = apply_idct(dctY, dctU, dctV)
+    run('config.m');
+
+    compY = ibdct(dctY, [8,8], [HEIGHT WIDTH]);
+    compU = dctU;
+    compV = dctV;
+end
