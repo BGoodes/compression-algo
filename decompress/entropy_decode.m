928723346989872e23f2b3aa2d8a44361763ac65
Julien Moullec <contact@julien-moullec.fr>
2025-03-23 12:21:14 +0100

    Import huffman lib

--- decompress/entropy_decode.m
+++ decompress/entropy_decode.m
@@ -1,3 +1,15 @@
+% function decoded = entropy_decode(encoded, height, width, dict)
+%
+% Perform entropy decoding on a component using Huffman coding
+% Inputs:
+%   encoded : Huffman encoded bitstream
+%   height : Height of the component
+%   width : Width of the component
+%   dict : Huffman dictionary
+%
+% Outputs:
+%   decoded : Huffman decoded component
+%
 function decoded = entropy_decode(encoded, height, width, dict)
     
     % Decode the bitstream
